daafc5c56f6fb65ab33fff5a54ffbbd355b8ed8a
Saifahmeed <122873799+Saifahmeed@users.noreply.github.com>
2023-04-24 23:39:48 +0200

    Create Airport.h

---
+++ Airport.h
@@ -0,0 +1,23 @@
+#ifndef Airport_H
+#define Airport_H
+
+#include "DEQ.h"
+
+#include "Plane.h"
+
+#include "Runway.h"
+
+// Airport class
+class Airport {
+  private: Runway runway; // runway object
+  DEQ < Plane * > landingQueue; // queue of planes waiting to land
+  int avgwt;
+  double landingProb; // probability of a plane arriving for landing
+  int time_max;
+  int enter_arrival_time;
+  int clock; // clock that counts the number of minutes simulated
+  public: Airport(int max, int entertime);
+  void simulate(int simulationTime);
+  double landingprob(); // random function generator
+  void planeArrives(Plane p);
+};
