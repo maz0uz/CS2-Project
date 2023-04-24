caac5d01bf897480348468e651beb29ca9997d37
Saifahmeed <122873799+Saifahmeed@users.noreply.github.com>
2023-04-24 23:31:29 +0200

    Update Runway.h

--- Runway.h
+++ Runway.h
@@ -2,25 +2,14 @@
 #define RUNWAY_H
 
 #include "DEQ.h"
+
 #include "Plane.h"
 
 class Runway {
-private:
-    DEQ<Plane*> landing_queue; // queue of planes waiting to land
-    int landing_time; // time taken for a plane to land
-    int currentt;
-    int t_rem_serv;
-    int time_left; // time left for the current plane in the landing process
-
-public:
-    Runway(int landing_time);
-    bool isBusy() const;
-    void land();
-    int sett_rem_serv(int t_rem_serv);
-    int gett_rem_serv();
-    int setcurrent_t(int currentt);
-    void tick(); // returning current time
-    void addToLandingQueue(Plane* plane);
+  private: int arrivaltime;
+  int time_landing;
+  public: Runway(double time_landingg);
+  bool Occupied(double arrivalt, double currenttime);
 };
 
 #endif
