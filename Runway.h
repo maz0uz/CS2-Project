6f59e4746b68b96419b542e239663c448cc698b8
HadiHesham <125379428+HadiHesham@users.noreply.github.com>
2023-04-24 14:34:42 -0700

    Update Runway.h

--- Runway.h
+++ Runway.h
@@ -1,14 +1,14 @@
 #ifndef RUNWAY_H
 #define RUNWAY_H
 
-#include "DEQ.h"
-
-#include "Plane.h"
-
 class Runway {
-  private: int arrivaltime;
+ private: 
+  int arrivaltime;
   int time_landing;
-  public: Runway(double time_landingg);
+ public: 
+  Runway(double time_landingg);
+  void settimelanding(int time);
+  int gettimelanding();
   bool Occupied(double arrivalt, double currenttime);
 };
 
