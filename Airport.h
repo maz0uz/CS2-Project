e37aa4cef716c24c3a69552d33c50c9596366b4f
HadiHesham <125379428+HadiHesham@users.noreply.github.com>
2023-04-24 16:24:35 -0700

    Update Airport.h

--- Airport.h
+++ Airport.h
@@ -11,13 +11,14 @@
 class Airport {
  private: 
   Runway runway; // runway object
-  DEQ < Plane > landingQueue; // queue of planes waiting to land
+  DEQ < Plane >* landingQueue; // queue of planes waiting to land
   int avgwt;
   double landingProb; // probability of a plane arriving for landing
   int time_max;
   int clock; // clock that counts the number of minutes simulated
  public: 
   Airport(int max);
+  ~Airport();
   void simulate(int simulationTime);
   double landingprob(); // random function generator
   void planeArrives(Plane p);
