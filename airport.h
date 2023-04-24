81113c12e263214462211bb4bce5e796e0178297
HadiHesham <125379428+HadiHesham@users.noreply.github.com>
2023-04-24 14:36:59 -0700

    Update airport.h

--- airport.h
+++ airport.h
@@ -1,61 +1,10 @@
-// Plane class
-class Plane {
-private:
- const int id; 
- static int count;
- int wtime;
- static int wait[count];
- int time_arrival; // setter and getters
-public:
- Plane(int ID) : id(ID);
- static double avgwait();
- void setwtime(int time);
- int getwtime();
- void settimearrival (int time);
- int gettimearrival ();
- int getId();
-};
-
-template <class T>;
-class DEQ{
-private:
- struct Node{
-  T Value;
-  Node* next;
-  Node* previous;
- };
- Node<T> *front;
- Node<T> *rear;
- int len;
-public:
- DEQ();
- ~DEQ();
- bool DEQisEmpty();
- void Add_Front(T data);
- void Add_Rear(T data);
- T Remove_Front();
- T Remove_Rear();
- T View_Front();
- T View_Rear();
- int DEQ_Length();
- void DisplayDEQ();
-};
-
-// Runway class
-class Runway {
-private:
-int arrivaltime;
-int time_landing;
-public:
-Runway(double time_landingg);
-bool Occupied(double arrivalt,double currenttime);
-};
-
 // Airport class
+#include "plane.h"
+#include "DEQ.h"
 class Airport {
 private:
 Runway runway; // runway object
-deque<Plane*> landingQueue; // queue of planes waiting to land
+DEQ landingQueue; // queue of planes waiting to land
 int avgwt;
 double landingProb; // probability of a plane arriving for landing
 int time_max; //maximum time the plane can stay in the queue before taking off (assuming the airport has a policy to avoid planes from waiting too long)
