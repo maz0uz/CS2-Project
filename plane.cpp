2ee73111a3d987e90cc57d3f8c328a12d6f7a9bf
HadiHesham <125379428+HadiHesham@users.noreply.github.com>
2023-05-11 11:04:16 -0700

    Add files via upload

---
+++ plane.cpp
@@ -0,0 +1,35 @@
+//
+//  plane.cpp
+//  m
+//
+//  Created by macbook on 11/05/2023.
+//
+
+#include "plane.hpp"
+Plane :: Plane() : id(numberofplanes){
+    wtime=0;
+    time_arrival=0;
+}
+double Plane :: avgwait(){
+    auto iter=wait.begin();
+    double sum=0;
+    for(iter=wait.begin();iter<wait.end();iter++)
+        sum=sum+*iter;
+    double avg=sum/numberofplanes;
+    return avg;
+}
+void Plane :: setwtime(double time){
+    wtime=time;
+}
+int Plane :: getwtime(){
+    return wtime;
+}
+void Plane :: settimearrival(double time){
+    time_arrival=time;
+}
+double Plane :: gettimearrival(){
+    return time_arrival;
+}
+int Plane :: getId(){
+    return id;
+}
