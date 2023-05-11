6f7584ce5af5661f40f48f70974a7ef4960e53be
HadiHesham <125379428+HadiHesham@users.noreply.github.com>
2023-05-11 11:03:46 -0700

    Update Plane.h

--- Plane.h
+++ Plane.h
@@ -1,19 +1,32 @@
+//
+//  plane.hpp
+//  m
+//
+//  Created by macbook on 11/05/2023.
+//
+
+#ifndef plane_hpp
+#define plane_hpp
+#include <vector>
+#include <stdio.h>
 #ifndef PLANE_H
 #define PLANE_H
+using namespace std;
 class Plane {
- private: 
-  const int id;
-  static int numberofplanes;
-  int wtime;
-  static int wait[count];
-  int time_arrival; 
- public: 
-  Plane(int ID): id(ID);
+ private:
+    const int id;
+    static int numberofplanes;
+    double wtime;
+    static vector<double> wait;
+    double time_arrival;
+ public:
+  Plane();
   static double avgwait();
-  void setwtime(int time);
+  void setwtime(double time);
   int getwtime();
-  void settimearrival(int time);
-  int gettimearrival();
+  void settimearrival(double time);
+  double gettimearrival();
   int getId();
 };
 #endif
+#endif /* plane_hpp */
